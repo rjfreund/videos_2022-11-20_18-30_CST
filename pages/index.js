69ef0d64c90604c0ccbbf5f03a2abfc34444a80f
R.J. Freund <robert.joseph.freund@gmail.com>
2022-11-21 22:28:47 -0600

    -

--- pages/index.js
+++ pages/index.js
@@ -7,17 +7,32 @@ export default function Home() {
     <>
       <Head>
         <title>R.J. Freund&apos;s Video Recordings of Singing</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet" />
       </Head>
-      <h1>R.J. Freund&apos;s Video Recordings of Singing</h1>
-      <div>
-        <p>On This Day by David Pomeranz</p>
-        <p>2022-11-19</p>
-        <video src="IMG_8428_x264.mp4" controls />
-      </div>
-      <div>
-        <p>Wonderful Tonight by Eric Clapton</p>
-        <p>2022-11-19</p>
-        <video src="IMG_8429_x264.mp4" autoPlay controls />
+      <div className='container text-center py-4'>
+        <h1 className='mb-4'>R.J. Freund&apos;s Video Recordings of Singing</h1>
+        <div className='d-flex flex-column mb-3 gap-5'>
+          <div className='card m-auto' style={{ width: "18rem" }}>
+            <video className='card-img-top' src="IMG_8428_x264.mp4" controls />
+            <div className='card-body'>
+              <h5 className='card-title text-center'>On This Day by David Pomeranz</h5>
+              <p className='card-text text-center'>2022-11-19</p>
+            </div>
+          </div>
+          <div className='card m-auto' style={{ width: "18rem" }}>
+            <video className='card-img-top' src="IMG_8429_x264.mp4" controls />
+            <div className='card-body'>
+              <h5 className='card-title text-center'>Wonderful Tonight by Eric Clapton</h5>
+              <p className='card-text text-center'>2022-11-19</p>
+            </div>
+
+
+          </div>
+        </div>
+
+
       </div>
     </>
   )
