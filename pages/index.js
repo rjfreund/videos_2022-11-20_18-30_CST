f9d242262633b9fb87742ae302a0ae0eb7b7aae4
R.J. Freund <robert.joseph.freund@gmail.com>
2022-11-21 20:46:36 -0600

    -

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,17 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <><h1>R.J. Freund&apos;s Video Recordings of Singing</h1>
-      <div className={styles.container}>
-
+    <>
+      <Head>
+        <title>R.J. Freund&apos;s Video Recordings of Singing</title>
+      </Head>
+      <h1>R.J. Freund&apos;s Video Recordings of Singing</h1>
+      <div>
         <p>On This Day by David Pomeranz</p>
         <p>2022-11-19</p>
         <video src="IMG_8428_x264.mp4" controls />
       </div>
-      <div className={styles.container}>
+      <div>
         <p>Wonderful Tonight by Eric Clapton</p>
         <p>2022-11-19</p>
         <video src="IMG_8429_x264.mp4" autoPlay controls />
